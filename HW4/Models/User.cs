5c5d2b4b456f4e40453615372afdf270bfc7b7fb
Saitama267 <85219557+Saitama267@users.noreply.github.com>
2023-05-01 16:30:46 +0200

    Everything work

--- HW4/Models/User.cs
+++ HW4/Models/User.cs
@@ -9,7 +9,7 @@ namespace HW4.Models
     public class User
     {
         [Required(ErrorMessage = "Please enter Email ")]
-        [RegularExpression(@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", ErrorMessage = "Please enter a valid email address")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$", ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
         [Required(ErrorMessage ="Please enter your name")]
         [RegularExpression(@"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$", ErrorMessage = "Please enter Valid name")]
